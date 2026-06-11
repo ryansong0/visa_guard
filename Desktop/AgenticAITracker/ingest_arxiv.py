b8e5ca386e81187f08106df5923c35b17dd2ad0e
Ryan Song <ryansong129@gmail.com>
2026-06-10 21:05:33 -0400

    inputted print statements for outputs for each paper

--- Desktop/AgenticAITracker/ingest_arxiv.py
+++ Desktop/AgenticAITracker/ingest_arxiv.py
@@ -6,3 +6,12 @@ def fetch_arxiv_papers(query, max_results = 3):
         max_results = max_results,
         sort_by = arxiv.SortCriterion.SubmittedDate
     )
+
+    print(f"--- Fetching the latest papers for: '{query}' ---\n")
+
+    for result in search.results():
+        print(f"Title: {result.title}")
+        print(f"Published: {result.published.date()}")
+        print(f"Summary: {result.summary[:150]}")
+        print(f"URL: {result.entry_id}")
+        print("-" * 50)
